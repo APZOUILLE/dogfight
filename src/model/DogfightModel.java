f23123decd61c4e00d40e181e1a53f7ed3391feb
Antonin <Antonin@LAPTOP-361C87PU.home>
2018-04-24 09:28:16 +0200

    Phase2 Début du package modele

---
+++ src/model/DogfightModel.java
@@ -0,0 +1,18 @@
+package model;
+import java.util.*;
+
+public class DogfightModel {
+	
+	private Sky sky;
+	private IMobile mobiles;
+	
+	public DogfightModel(){}
+	public IArea getArea(){}
+	public void buildArea(Dimension dimension){}
+	public void addMobile(IMobile Mobile){}
+	public void removeMobile(IMobile Mobile){}
+	public ArrayList<IMobile> getMobiles(){}
+	public IMobile getMobileByPlayer(int player){}
+	public void setMobilesHavesMoved(){}
+	
+}
